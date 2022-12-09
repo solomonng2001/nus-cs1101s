827631430aa64f00861fbc1ff9853e000e6cbeff
Solomon Ng <66112309+solomonng2001@users.noreply.github.com>
2022-12-09 20:15:35 +0800

    Create elements_of_programming.js

---
+++ paths/elements_of_programming.js
@@ -0,0 +1,34 @@
+// Question 1
+// 2 + 3
+
+
+// Question 2
+// 25
+
+
+// Question 3
+const foo = 10;
+const bar = 10 + 20;
+// your solution goes here
+const foobar = foo + bar;
+
+
+// Question 4
+// your solution goes here
+function bar() {
+    return 20;
+}
+
+
+// Question 5
+function square(x) {
+    return x * x;
+}
+
+// your solution goes here
+function abs(x) {
+    return x >= 0 ? x : - x;
+}
+function square_of_difference(x, y) {
+    return abs(x - y) * abs(x - y);
+}
