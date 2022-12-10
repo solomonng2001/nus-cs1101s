d28699d97d19d4d2037b7df9d177f094cedcbc6c
Solomon Ng <66112309+solomonng2001@users.noreply.github.com>
2022-12-10 23:11:22 +0800

    Create studio_s4_in_class.js

---
+++ studios/studio_s4_in_class.js
@@ -0,0 +1,20 @@
+// Question 1
+// What is the result of compose(math_sqrt, math_log)(math_E)?
+// 1
+// What is the result of compose(math_log, math_sqrt)(math_E * math_E)?
+// 1
+
+
+// Question 2
+// 2
+
+
+// Question 3
+// 27
+
+
+// Question 4
+// a) 33
+// b) compose
+// c) 1
+// d) 2^2^27
